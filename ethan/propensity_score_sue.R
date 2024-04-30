3634c9b220ba5e2037b0da8aff7c8bfb68b8097a
jbrocker1 <jkbrocker1@gmail.com>
2024-04-30 13:45:42 -0500

    propensity score work

--- ethan/propensity_score_sue.R
+++ ethan/propensity_score_sue.R
@@ -1,9 +1,12 @@
 # Load necessary libraries
+if (!require("MatchIt")) install.packages("MatchIt")
 library(MatchIt)
+
+if (!require("dplyr")) install.packages("dplyr")
 library(dplyr)
 
 #load gss.RData
-dat <- read.csv("data/clean_data.csv")
+dat <- read.csv("../data/clean_data.csv")
 
 #Outcome-model Standardization
 lmod <- glm(cancer ~ hrt + race + Hispanic + surgmeno, family = binomial,data = dat)
@@ -31,6 +34,11 @@ EY0 <- beta[1]
 EY0
 EY1 <- beta[1] + beta[2]
 EY1
+print("Exposure-model Standarization")
+print("EY0")
+print(EY0)
+print("EY1")
+print(EY1)
 
 #Propensity-score Distributions
 a <- range(density(e[dat$hrt == 1], bw = .05)$y)
@@ -71,39 +79,62 @@ EY1
 rd <- EY1 - EY0
 rd
 
-#Using the Propensity Score in the Outcome Model (Conditional Treatment Effect Estimates)
-summary(glm(formula = cancer ~ hrt + e, family = binomial, data = dat))
+print("Propensity score in Outcome Model")
+print("EY0")
+print(EY0)
+print("EY1")
+print(EY1)
+print("RD")
+print(rd)
 
+#Using the Propensity Score in the Outcome Model (Conditional Treatment Effect Estimates)
+print("Propensity Score in Outcome MOdel (Conditional Treatment Effect Estimates")
+summary(glm(formula = cancer ~ hrt + e, family = binomial, data = dat))
 
 #Stratification on the Propensity Score
 out<-glm(hrt~race + Hispanic + surgmeno, family=binomial, data=dat)
 eb=fitted(out)
-quartiles <- quantile(eb, c(0,.5, .75, 1)) # 25 and 50 are same, causes error
+quartiles <- quantile(eb, c(0,.5, 1)) # 25 and 50 are same, causes error
 
-quartiles
-
+#equartiles <-cut(eb, breaks = quartiles, include.lowest = T)
 equartiles <-cut(eb, breaks = quartiles, include.lowest = T)
 
 out <- glm(cancer ~ hrt * equartiles - 1 - hrt, data = dat)
 
-EY0 <- out$coef[1:4]
-EY1 <- out$coef[1:4] + out$coef[5:8]
+print("summary(out)")
+print(summary(out))
+# WHY ALL THIS ITERATION?
+print("out$coef")
+print(out$coef)
+EY0 <- out$coef[1:2]
+EY1 <- out$coef[1:2] + out$coef[3:4]
+
+print("EY0")
+print(EY0)
+print("EY1")
+print(EY1)
+
+print("EY1-EY0")
+print(EY1-EY0)
 
 RD <- mean(c(EY1 - EY0))
-RD
+
+print("RD")
+print(RD)
 
 for (i in 1:3){
-  print(c(EY0[i],EY1[i]))
+  #print(c(EY0[i],EY1[i]))
 }
 
 
+if(TRUE) {
+	#Matching on the Propensity Score
+	if (!require("Matching")) install.packages("Matching")
+	library(Matching)
 
-#Matching on the Propensity Score
-if (!require("Matching")) install.packages("Matching")
-library(Matching)
+	print(summary(Match(Y=dat$cancer,Tr=dat$hrt,X=e,estimand="ATE",caliper=.25,replace=T,ties=F)))
+	sqrt(var(e))
 
-summary(Match(Y=dat$cancer,Tr=dat$hrt,X=e,estimand="ATE",caliper=.25,replace=T,ties=F))
-sqrt(var(e))
-
-summary(Match(Y=dat$cancer,Tr=dat$hrt,X=e,estimand="ATT",caliper=.25,replace=T,ties=F))
-MatchBalance(cancer~race + Hispanic + surgmeno, data=dat, match.out=match.out)
+	print(summary(Match(Y=dat$cancer,Tr=dat$hrt,X=e,estimand="ATT",caliper=.25,replace=T,ties=F)))
+	MatchBalance(cancer~race + Hispanic + surgmeno, data=dat, match.out=match.out)
+}
